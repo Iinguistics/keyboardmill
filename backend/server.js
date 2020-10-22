0e12aa388fd6c23befd9c70a37f1a4291ac0ec56
Linguistics <thylinguistics@gmail.com>
2020-10-21 23:38:21 -0700

    added middleware/error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const products = require('./data/products');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const productRoutes = require('./routes/productRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -11,18 +16,17 @@ app.get("/", (req,res)=>{
     res.send("api was hit")
 });
 
-app.get("/api/products", (req,res)=>{
-    res.json(products);
-});
 
-app.get("/api/products/:id", (req,res)=>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
+
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightCyan.inverse)
 );
 
