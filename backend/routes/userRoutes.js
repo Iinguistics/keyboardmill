c93dd5fdac565df9cf258ec051486c14935ff75d
Linguistics <thylinguistics@gmail.com>
2020-10-31 21:45:07 -0700

    added PUT route to update user info

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { authUser, getUserProfile, registerUser } = require('../controllers/userController');
+const { authUser, getUserProfile, registerUser, updateUserProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/authMilldeWare');
 
 router.post("/login", authUser);
 router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
 router.post("/register", registerUser);
 
 
