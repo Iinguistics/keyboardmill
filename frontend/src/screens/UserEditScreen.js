0c08e086268fa7a2516db9ea65727b9b05be2f77
Linguistics <thylinguistics@gmail.com>
2020-11-10 21:36:07 -0800

    added edit form & comp lvl state

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Form, Button, Row, Col, Table } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserEdit } from '../actions/userActions';
@@ -8,39 +8,84 @@ import { listMyOrders } from '../actions/orderActions';
 import Loader from '../components/bootstrapHelpers/Loader';
 import Message from '../components/bootstrapHelpers/Message';
 import { USER_UPDATE_RESET } from '../actions/types';
+import FormContainer from '../components/FormContainer';
 
 
 const UserEditScreen = ({ match, history }) => {
-
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [isAdmin, setIsAdmin] = useState(false);
 
     const userLogin = useSelector(state => state.userLogin);
     const{ userInfo } = userLogin;
 
     const dispatch = useDispatch();
     const userEdit = useSelector(state => state.userEdit);
-    const { loading, error, user } = userEdit;
+    const { loading:getUserLoading, error:getUserError, user } = userEdit;
 
 
     useEffect(()=>{
         if(!userInfo || !userInfo.isAdmin){
             history.push('/');
         }
-     dispatch(getUserEdit(match.params.id));
+        if(!user.name || user._id !== match.params.id){
+            dispatch(getUserEdit(match.params.id));
+        }else{
+            setName(user.name)
+            setEmail(user.email)
+        }
 
-    },[])
+    },[user, dispatch, match.params.id])
+
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        
+    }
 
 
 
 
     return (
-        <div className="my-5">
-            {user && (
-                <div>
-                  <h2>{user.name}</h2>
-                  <h2>{user.email}</h2>
-                </div>
+        <Fragment>
+            <Link to='/admin/userlist' className="btn btn-light my-5">
+                Go Back
+            </Link>
+
+            <FormContainer>
+            <h1>Edit User</h1>
+            {getUserLoading ? <Loader /> : getUserError ? <Message variant="danger">{getUserError}</Message> : ( 
+            <Form onSubmit={submitHandler}>
+
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name"
+                 value={name} 
+                 onChange={(e)=> setName(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email"
+                 value={email} 
+                 onChange={(e)=> setEmail(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="isadmin">
+                <Form.Check type="checkbox" label="Is Admin"
+                 checked={isAdmin} 
+                 onChange={(e)=> setIsAdmin(e.target.checked)} />
+            </Form.Group>
+
+
+             <Button variant="primary" type="submit" id="user-update">
+                Update
+            </Button>
+        </Form>
             )}
-        </div>
+            
+        </FormContainer>
+        </Fragment>
     )
 }
 
