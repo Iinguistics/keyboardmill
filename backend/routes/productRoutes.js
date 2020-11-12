699cfd0b60e63210cb57ec214336df3782e8cce9
Linguistics <thylinguistics@gmail.com>
2020-11-12 02:26:24 -0800

    added delete product route for admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductById } = require('../controllers/productController');
+const { getProducts, getProductById, deleteProduct } = require('../controllers/productController');
+const { protect, admin } = require('../middleware/authMilldeWare');
 
 
 router.get("/", getProducts);
 router.get("/:id", getProductById );
+router.delete('/remove/:id', protect,admin, deleteProduct);
+
 
 
 
