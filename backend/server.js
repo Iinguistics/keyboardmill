caaafa0c1c434a676b9c6a98c87828c1d9740aa4
Linguistics <thylinguistics@gmail.com>
2020-11-15 16:05:47 -0800

    added multer package for file uploads, route,config

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require('path');
 const colors = require('colors');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -25,8 +27,12 @@ app.get("/", (req,res)=>{
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
 
 app.get('/api/config/paypal', (req,res)=> res.send(process.env.PAYPAL_CLIENT_ID));
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 
