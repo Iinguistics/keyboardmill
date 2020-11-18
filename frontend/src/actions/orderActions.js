fd968c6e43af58526d1f28b208529c44b065c312
Linguistics <thylinguistics@gmail.com>
2020-11-18 14:58:18 -0800

    adding {} object for put req, was not registering the headers/ fixed issue

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -174,13 +174,12 @@ export const createOrder = (order)=> async (dispatch, getState)=>{
 
        const config = {
         headers:{
-            'Content-Type': 'application/json',
             Authorization: `Bearer ${userInfo.token}`
         }
     }
  
       
-       const { data } = await axios.put(`/api/orders/${id}/delivered`, config)
+       const { data } = await axios.put(`/api/orders/${id}/delivered`, {}, config)
  
        dispatch({
            type: ORDER_DELIVERED_SUCCESS,
